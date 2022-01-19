91f140617b6545da401a5a61757cebc8ab1e28b4
alan <alanarostegui@hotmail.com>
2022-01-18 23:20:52 -0600

    feat(base-blog-em): creating queries with useQuery

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,7 +16,11 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data } = useQuery("posts", fetchPosts);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
