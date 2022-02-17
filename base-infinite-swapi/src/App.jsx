c760768b03972a59cc355397b582a90363dae238
alan <alanarostegui@hotmail.com>
2022-02-17 00:51:20 -0600

    feat: add react query to app

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
+
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <ReactQueryDevtools />
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+    </QueryClientProvider>
   );
 }
 
