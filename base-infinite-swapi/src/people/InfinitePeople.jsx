e57fa2aaaeeaba95e5501d65a6482e1f343c6003
alan <alanarostegui@hotmail.com>
2022-02-17 15:21:47 -0600

    feat(sw): write useInfinityQuery call

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -9,5 +10,10 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    { getNextPageParam: (lastPage) => lastPage.next || undefined }
+  );
   return <InfiniteScroll />;
 }
